ff7990721ca3d7bfdab3f2d35798a82586d07863
MuattarNishonova <146547489+MuattarNishonova@users.noreply.github.com>
2023-10-13 09:37:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function showText() {
+    document.querySelector('.showhide').classList.add('show')
+}
+
+
+function hideText() {
+    document.querySelector('.showhide ').classList.remove('show')
+}
+function showText2() {
+    document.querySelector('.showhide2').classList.add('show')
+}
+
+
+function hideText2() {
+    document.querySelector('.showhide2 ').classList.remove('show')
+}
+function showText3() {
+    document.querySelector('.showhide3').classList.add('show')
+}
+
+
+function hideText3() {
+    document.querySelector('.showhide3 ').classList.remove('show')
+}
+// function secondpage(){
+    
+// }
